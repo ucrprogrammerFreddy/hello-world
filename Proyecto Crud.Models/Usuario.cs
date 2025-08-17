24c83a207c3f009f0eae114d3532b9a6d5d6a8b4
Freddy005 <freddysz895@gmail.com>
2025-08-17 16:05:43 -0600

    se agregó otro usuario

--- Proyecto Crud.Models/Usuario.cs
+++ Proyecto Crud.Models/Usuario.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 
 namespace ProyectoCrud.Model
 {
+
     public class Usuario
     {
+
         public int IdUsuario { get; set; }
 
         public String? Nombre { get; set; }
 
         public String fechaNacimiento { get; set; }
 
-
-
+        public String OtroUsuario { get; set; }
 
     }
 }
