0285ac71953ebad270b2fca58355cf6fc169f5d7
fsanchez <freddy.sanchezzamora@ucr.ac.cr>
2025-08-29 15:46:34 -0600

    se fusiono otra vez escritorio local 2 en main

---
+++ Proyecto Crud.Models/PruebaEscritorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Crud.Models
+{
+    internal class PruebaEscritorio
+    {
+        public string nombreEscritorioprueba { get; set; }
+    }
+}
