dc9f593dc03c57daec3c999c512289d238a42620
fsanchez <freddy.sanchezzamora@ucr.ac.cr>
2025-08-28 20:30:57 -0600

    se agregó el objeto Escritorio1

---
+++ Proyecto Crud.Models/Escritorio1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Crud.Models
+{
+   public class Escritorio1
+    {
+
+
+           public string Escritorio1Prop { get; set; }
+           public string nombre { get; set; }
+          public string codigo { get; set; }
+          
+
+    }
+}
